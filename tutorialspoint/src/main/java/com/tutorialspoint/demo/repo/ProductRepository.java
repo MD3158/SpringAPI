88a76e55bb6953a70065da52b4e883de95eed80f
MD3158 <92357923+MD3158@users.noreply.github.com>
2021-11-05 09:00:48 +0700

    Delete tutorialspoint directory

--- tutorialspoint/src/main/java/com/tutorialspoint/demo/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tutorialspoint.demo.repo;
-
-import com.tutorialspoint.demo.model.Product;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends CrudRepository<Product, Integer> {
-}
